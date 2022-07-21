dfe2736cd8ed58a65c841dcc74ff39d22b222ff3
richard1015 <51844712@qq.com>
2022-07-21 15:12:55 +0800

    chore: jdt build file hash

--- vite.config.jdt.ts
+++ vite.config.jdt.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import config from './src/docs_vue/config.json';
 import { compressText } from './src/components/demo-block/basedUtil';
 const hljs = require('highlight.js'); // https://highlightjs.org/
-
+const refRandom = Math.random().toString(36).slice(-8);
 const resolve = path.resolve;
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -95,16 +95,16 @@ export default defineConfig({
   build: {
     target: 'es2015',
     outDir: './dist/jdt/',
-    assetsDir: config.version,
+    assetsDir: `${config.version}-${refRandom}`,
     cssCodeSplit: true,
     rollupOptions: {
       input: {
         vue: resolve(__dirname, 'index.jdt.html')
       },
       output: {
-        entryFileNames: `${config.version}/[name].js`,
-        chunkFileNames: `${config.version}/[name].js`,
-        assetFileNames: `${config.version}/[name].[ext]`
+        entryFileNames: `${config.version}-${refRandom}/[name].js`,
+        chunkFileNames: `${config.version}-${refRandom}/[name].js`,
+        assetFileNames: `${config.version}-${refRandom}/[name].[ext]`
       }
     }
   }
